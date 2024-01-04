331d1d44bc3d0c106349127341a2b82e29d837c1
Igor Zielosko <igorzielosko.iz@gmail.com>
2024-01-04 19:09:08 -0300

    Criação de arquivo com o zod para ler as váriaveis de ambiente e usar isso no nest com o nest config, colocando como uma importação no arquivo main e setando como um modulo global para a aplicação

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+import { number, z } from "zod";
+
+export const envSchema = z.object({
+    DATABASE_URL: z.string().url(),
+    PORT: z.coerce.number().optional().default(3333),
+})
+
+export type Env = z.infer<typeof envSchema>
