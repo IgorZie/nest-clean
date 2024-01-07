e29737bb715e0d8dc596d1ce681be9d46dec66e3
Igor Zielosko <igorzielosko.iz@gmail.com>
2024-01-07 18:10:12 -0300

    Criação de Controller para buscar as perguntas mais recentes e como query params de opção para paginar a busca

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,20 +1,52 @@
-import { Controller, Post, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
-import { Request } from "express";
-import { CurrentUser } from "src/auth/current-user-decorator";
-import { UserPayload } from "src/auth/jwt.strategy";
+import { CurrentUser } from "../auth/current-user-decorator";
+import { UserPayload } from "../auth/jwt.strategy";
+import { z } from "zod";
+import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
+import { PrismaService } from "../prisma/prisma.service";
+
+const createQuestionBodySchema = z.object({
+    title: z.string(),
+    content: z.string()
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-    constructor() {}
+    constructor(
+        private prisma: PrismaService
+    ) {}
 
     @Post()
-    @UseGuards(JwtAuthGuard)
-    async handle(@CurrentUser() user: UserPayload) {
-        console.log(user.sub)
-        
-        return 'ok' 
+    async handle(
+        @Body(new ZodValidationPipe(createQuestionBodySchema)) body: CreateQuestionBodySchema,
+        @CurrentUser() user: UserPayload
+    ) {
+        const { title, content } = body
+        const { sub: userId } = user
+
+        const slug = this.convertioStug(title)
+
+        await this.prisma.question.create({
+            data: {
+                authorId: userId,
+                title,
+                content,
+                slug
+            }
+        })
+    }
+
+    private convertioStug(title: string): string {
+        return title
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u836f]/g, '')
+        .replace(/[^\w\s-]/g, '')
+        .replace(/\s+/g, '-')
     }
 
 }
